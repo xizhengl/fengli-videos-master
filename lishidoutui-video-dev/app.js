2cbf1c0db3068aed714eced6647f56a289e0b310
lixizheng123 <2425220864@qq.com>
2020-06-16 16:02:35 +0800

    小程序工程文件

---
+++ lishidoutui-video-dev/app.js
@@ -0,0 +1,5 @@
+//app.js
+App({
+  serverUrl:"http://4fcde8a42d20.ngrok.io",
+  userInfo: null
+})
---
+++ lishidoutui-video-dev/pages/userRegist/regist.js
@@ -0,0 +1,50 @@
+var app = getApp()
+
+Page({
+    data: {
+    },
+    doRegist: function(e){
+        var password = e.detail.value.password;
+        var username = e.detail.value.username;
+        if(password.length == 0 || username.length == 0){
+            wx.showToast({
+              title: '用户名或密码不能为空',
+              icon: 'none',
+              duration: 3000
+            })
+        }else{
+            wx.request({
+              url: app.serverUrl + "/regist",
+              method: "POST",
+              data: {
+                username: username,
+                password: password
+              },
+              header: {
+                'content-type': 'application/json' // 默认值
+              },
+              success: function(res){
+                var status = res.data.status;
+                  if(status == 200){
+                    wx.showToast({
+                        title: "用户注册成功~！！！",
+                        icon: 'none',
+                        duration: 3000
+                      })
+                  }else if(status == 500){
+                    wx.showToast({
+                        title: res.data.msg,
+                        icon: 'none',
+                        duration: 3000
+                      })
+                  }
+              }
+            })
+        }
+    },
+    goLoginPage:function() {
+        wx.navigateTo({
+          url: '../userLogin/login',
+        })
+      }
+})
